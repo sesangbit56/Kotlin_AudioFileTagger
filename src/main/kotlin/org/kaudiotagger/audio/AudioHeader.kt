2e617a41c92cbde74dfde7cee22531d57a4965e3
sesangbit56 <sesangbit56@gmail.com>
2020-06-25 12:26:57 +0900

    20200625 - AudioHeader.kt completed

--- src/main/kotlin/org/kaudiotagger/audio/AudioHeader.kt
+++ src/main/kotlin/org/kaudiotagger/audio/AudioHeader.kt
@@ -1,4 +1,38 @@
 package org.kaudiotagger.audio
 
 interface AudioHeader {
+
+    var encodingType : String?
+
+    var byteRate : Int?
+
+    var bitRateAsNumber : Long?
+
+    var audioDataLength : Long?
+
+    var audioDataStartPosition : Long?
+
+    var audioDataEndPosition : Long?
+
+    var sampleRateAsNumber : Int?
+
+    var sampleRate : String?
+
+    var format : String?
+
+    var channels : String?
+
+    var variableBitRate : Boolean
+
+    var trackLength : Int?
+
+    var preciseTrackLength : Double?
+
+    var bitsPerSample : Int?
+
+    var isLossless : Boolean?
+
+    var getNoOfSamples : Long?
+
+
 }
