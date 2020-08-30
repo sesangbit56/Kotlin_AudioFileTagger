0eb2020b6251a78d3b2050bf3abc8105ce8e2175
sesangbit56 <sesangbit56@gmail.com>
2020-08-30 21:18:05 +0900

    #28 add WavOptions.kt

---
+++ src/main/kotlin/org/kaudiotagger/audio/wav/WavOptions.kt
@@ -0,0 +1,12 @@
+package org.kaudiotagger.audio.wav
+
+enum class WavOptions {
+    READ_ID3_ONLY,
+    READ_INFO_ONLY,
+    READ_ID3_UNLESS_ONLY_INFO,
+    READ_INFO_UNLESS_ONLY_ID3,
+    READ_ID3_ONLY_AND_SYNC,
+    READ_ID3_UNLESS_ONLY_INFO_AND_SYNC,
+    READ_INFO_UNLESS_ONLY_ID3_AND_SYNC;
+
+}
