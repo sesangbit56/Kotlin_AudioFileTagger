4beeb8e7f706b4d2bc0616b00cbf2c35e0355db9
sesangbit56 <sesangbit56@gmail.com>
2020-08-30 21:51:14 +0900

    #29 add WavSaveOptions.kt

---
+++ src/main/kotlin/org/kaudiotagger/audio/wav/WavSaveOptions.kt
@@ -0,0 +1,9 @@
+package org.kaudiotagger.audio.wav
+
+enum class WavSaveOptions {
+    SAVE_EXISTING_AND_ACTIVE,
+    SAVE_ACTIVE,
+    SAVE_BOTH,
+    SAVE_EXISTING_AND_ACTIVE_AND_SYNC,
+    SAVE_BOTH_AND_SYNC;
+}
